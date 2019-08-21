e9eeefa759e0ba3227cc5745d87550f9459fcd3d
Tjeu Foolen <12643433+tjeufoolen@users.noreply.github.com>
2019-08-21 22:06:05 +0200

    changed connection message to only donators

--- src/main/java/dev/foolen/survival/modules/connectionmessages/listeners/PlayerJoin.java
+++ src/main/java/dev/foolen/survival/modules/connectionmessages/listeners/PlayerJoin.java
@@ -1,5 +1,7 @@
 package dev.foolen.survival.modules.connectionmessages.listeners;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -7,6 +9,12 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class PlayerJoin implements Listener {
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent e) {
-        e.setJoinMessage(null);
+        Player p = e.getPlayer();
+
+        if (p.hasPermission("group.vip") || p.hasPermission("group.vip+")) {
+            e.setJoinMessage(ChatColor.GOLD + p.getName() + " joined the game!");
+        } else {
+            e.setJoinMessage(null);
+        }
     }
 }
--- src/main/java/dev/foolen/survival/modules/connectionmessages/listeners/PlayerQuit.java
+++ src/main/java/dev/foolen/survival/modules/connectionmessages/listeners/PlayerQuit.java
@@ -1,13 +1,20 @@
 package dev.foolen.survival.modules.connectionmessages.listeners;
 
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class PlayerQuit implements Listener {
     @EventHandler
     public void onPlayerQuitEvent(PlayerQuitEvent e) {
-        e.setQuitMessage(null);
+        Player p = e.getPlayer();
+
+        if (p.hasPermission("group.vip") || p.hasPermission("group.vip+")) {
+            e.setQuitMessage(ChatColor.GOLD + p.getName() + " left the game!");
+        } else {
+            e.setQuitMessage(null);
+        }
     }
 }
